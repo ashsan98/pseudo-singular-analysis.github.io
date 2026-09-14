20eed0d2a928f62fcf47f652a111c53ebbdb03e4
ashsan98 <itsashsan98@gmail.com>
2026-09-14 11:26:34 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     { name: 'Karsten Bohlen', affiliation: 'Universität Regensburg', website: 'https://sites.google.com/view/kbohlen/start' },
     { name: 'Alessandro Pietro Contini', affiliation: 'Universität Hannover', website: 'https://www.analysis.uni-hannover.de/institut/personenverzeichnis/personen-detailansicht/institut-fuer-analysis/wissenschaftliche-mitarbeiterinnen-und-mitarbeiter-2/alessandro-pietro-contini' },
     { name: 'Ksenia Fedosova', affiliation: 'Universität Münster', website: 'https://www.uni-muenster.de/FB10/Service/show_perspage.shtml?id=1783' },
-    { name: 'Daniel Grieser', affiliation: 'Universität Oldenburg', website: 'https://uol.de/daniel-grieser' },
+    { name: 'Daniel Grieser', affiliation: 'Universität Oldenburg', website: 'https://uol.de/daniel-grieser', slides: "/notes_grieser.pdf" },
     { name: 'Gayana Jayasinghe', affiliation: 'Università di Roma', website: 'https://sites.google.com/view/gayanajayasinghe/home' },
     { name: 'Mirela Kohr', affiliation: 'Babeş-Bolyai University', website: 'https://math.ubbcluj.ro/~mkohr/'},
     { name: 'Lucas Lemoine', affiliation: 'Université Paris Est Créteil', website: 'https://lama-umr8050.fr/membres/lemoine.lucas' },
@@ -129,7 +129,7 @@ function Home() {
                   <span> &middot; </span>
                   <span style={{ fontWeight: 500 }}>
                     <a href={speaker.slides} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'underline' }}>
-                      Slides
+                      Slides/Notes
                     </a>
                   </span>
                 </>
